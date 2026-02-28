0b2cefcfa71561bf06f53e6e7fc6729e55fa4399
rebeacham14 <70616503+rebeacham14@users.noreply.github.com>
2026-02-28 13:56:02 -0800

    Change server port from 3000 to 8080

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000; // --> .env file
+const PORT = 8080; // --> .env file
 
 // Define a basic GET route
 app.get('/', (req, res) => {
