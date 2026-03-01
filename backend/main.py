2a7d5c1f3e5f348b2e8f985bdb615df1c05662c5
comnk <vhsiao36@gmail.com>
2026-02-28 16:08:27 -0800

    basic api and router setup

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from routers import routes
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(routes.router)
---
+++ backend/routers/routes.py
@@ -0,0 +1,8 @@
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/api", tags=["api"])
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
